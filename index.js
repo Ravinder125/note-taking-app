35640c43144eec7b7b96e104d1e85b8a6f469f1a
Ravinder <ravinder729090@gmail.com>
2025-01-05 18:15:36 +0530

    create server using express

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ const express = require('express');
 const app = express();
 
 
-// const PORT = 
+app.set('view engine', 'ejs');
+
+// Port
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res)=>{
+    res.send('Hello World');
+})
+
+
+
+app.listen(PORT, (req, res)=>{
+    console.log(`Server is running on port ${PORT}`);
+})
